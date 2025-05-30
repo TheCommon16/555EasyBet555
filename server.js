8b44cf2bea0a8a6dfb4562df346432fd5f07860a
TheCommon16 <84679237+TheCommon16@users.noreply.github.com>
2025-05-30 22:05:10 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,356 @@
+const express = require('express');
+const cors = require('cors');
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+
+
+
+
+app.use(cors({
+    origin: ["http://192.168.1.1"]
+}));
+app.use(express.json());// Middleware to parse JSON bodies
+
+app.use(express.urlencoded({ extended: true }));// Middleware to parse URL-encoded bodies (for form submissions)
+
+app.use(express.static("loginpages")); //piglia dalla cartella pages e manda al client
+app.use(express.static(path.join(__dirname, "loginpages")));  // Serve loginpages as static
+app.use(express.static(path.join(__dirname, "leaderboardpages")));  // Serve loginpages as static
+app.use(express.static(path.join(__dirname, "slot1")));       
+app.use(express.static(path.join(__dirname, "slot2")));      
+app.use(express.static(path.join(__dirname, "coinflip3")));      
+
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "utentislot"
+  });
+
+app.post("/leaderboardmoney",(req,res) => {
+     let updateSql = "SELECT username, money FROM utenti ORDER BY money DESC LIMIT 10";
+        con.query(updateSql, function (err, result) {
+            if (err) {
+                console.error("Query error:", err);
+                return;
+            }
+
+            
+           
+
+            res.json({ tabella: result }); // Send JSON response
+        });
+
+
+
+});
+
+app.post("/coinflip", (req, res) => {
+    const userId = req.body.id;
+    const userbetsizee = Number(req.body.userbetsize);
+    
+
+    console.log("Coin Flip");
+    console.log("User ID: "+ userId);
+    console.log("User Betsize: "+ userbetsizee);
+
+    num1coinflip = Math.floor(Math.random() * 2) + 1;
+    usermoneywin = 0;
+    if(num1coinflip == 1){
+        usermoneywin = userbetsizee * 2 ;
+        
+    }
+
+
+        let sql = "SELECT * FROM utenti WHERE ID = ?";
+    con.query(sql, [userId], function (err, result) {
+        if (err) {
+            console.error("Query error:", err);
+            return;
+        }
+        else if (result[0].money < userbetsizee){
+            console.error("No soldi coin flip");
+            return;
+        }
+    
+        var userDBmoney = result[0].money;
+        if(usermoneywin > 0){
+            userDBmoneyupdate = userDBmoney + userbetsizee;
+      
+        }
+        else{
+            userDBmoneyupdate = userDBmoney - userbetsizee;
+            usermoneywin = -(userbetsizee)
+        }
+
+        // UPDATE query
+        let updateSql = "UPDATE utenti SET money = ? WHERE ID = ?";
+        con.query(updateSql, [userDBmoneyupdate, userId], function (err, result) {
+            if (err) {
+                console.error("Query error:", err);
+                return;
+            }
+
+                           /*let spinlog = num1+":"+num2+":"+num3;
+                            let updateSql = "INSERT logspin (slottype, logspin, userbetsize, userbalance, userwin,usernewbalance, id_user) VALUES (?,?,?,?,?,?,?)";
+                            con.query(updateSql, [slottype, spinlog, userbetsizee,userDBmoney,usermoneywin,userDBmoneyupdate,userId], function (err) {
+                                if (err) {
+                                    console.error("Query error:", err);
+                                    return;
+                                }
+                                console.log("Log Spin Added");
+                                
+                            
+                            });*/
+            
+            console.log("New Bal: ", userDBmoneyupdate);
+
+            res.json({ first:num1coinflip, moneywin: userDBmoneyupdate }); // Send JSON response
+        });
+    });
+
+
+});
+
+
+// gestire i POST
+app.post("/spin", (req, res) => {
+    const userId = req.body.id;
+    const userbetsizee = Number(req.body.userbetsize);
+    //var slottype = 1;
+    console.log("Slot");
+    console.log("User ID: "+ userId);
+    console.log("User Betsize: "+ userbetsizee);
+
+
+      
+        num1 = Math.floor(Math.random() * 16) + 1;
+        num2 = Math.floor(Math.random() * 16) + 1;
+        num3 = Math.floor(Math.random() * 16) + 1;
+
+
+
+
+        if(num1 == num2 || num2 == num3 || num1 == num3){
+            num1 = Math.floor(Math.random() * 16) + 1;
+            num2 = Math.floor(Math.random() * 16) + 1;
+            num3 = Math.floor(Math.random() * 16) + 1;
+        }
+
+        numJ = Math.floor(Math.random() * 100) + 1;
+        if(numJ == 55){
+            num1 = 55;
+
+        }
+        numJ = Math.floor(Math.random() * 100) + 1;
+        if(numJ == 55){
+            num2 = 55;
+
+        }
+        numJ = Math.floor(Math.random() * 100) + 1;
+        if(numJ == 55){
+            num3 = 55;
+
+        }
+
+
+        
+    
+    var usermoneywin = 0;
+    if(num1 == num2 && num2 == num3){
+        if(num1 == 55){
+              usermoneywin = userbetsizee * 100000;
+        }
+        else{    
+            usermoneywin = userbetsizee * 1000;
+        }
+        
+    }
+    else if(num1 == num2 || num1 == num3 || num2 == num3){
+        if(num1 == 55 && num2 == 55 || num1 == 55 && num3 == 55 || num2 == 55 && num3 == 55){
+            usermoneywin = userbetsizee * 5000;
+        }
+        else{
+            usermoneywin = userbetsizee * 25;
+        }
+    }
+
+
+    let sql = "SELECT * FROM utenti WHERE ID = ?";
+    con.query(sql, [userId], function (err, result) {
+        if (err) {
+            console.error("Query error:", err);
+            return;
+        }
+        else if (result[0].money < userbetsizee){
+
+            return;
+        }
+    
+        var userDBmoney = result[0].money;
+        if(usermoneywin > 0){
+            userDBmoneyupdate = userDBmoney + usermoneywin;
+        }
+        else{
+            userDBmoneyupdate = userDBmoney - userbetsizee;
+            usermoneywin = -(userbetsizee)
+        }
+
+        // UPDATE query
+        let updateSql = "UPDATE utenti SET money = ? WHERE ID = ?";
+        con.query(updateSql, [userDBmoneyupdate, userId], function (err, result) {
+            if (err) {
+                console.error("Query error:", err);
+                return;
+            }
+
+                           /*let spinlog = num1+":"+num2+":"+num3;
+                            let updateSql = "INSERT logspin (slottype, logspin, userbetsize, userbalance, userwin,usernewbalance, id_user) VALUES (?,?,?,?,?,?,?)";
+                            con.query(updateSql, [slottype, spinlog, userbetsizee,userDBmoney,usermoneywin,userDBmoneyupdate,userId], function (err) {
+                                if (err) {
+                                    console.error("Query error:", err);
+                                    return;
+                                }
+                                console.log("Log Spin Added");
+                                
+                            
+                            });*/
+            
+            console.log("New Bal: ", userDBmoneyupdate);
+
+            res.json({ first:num1, second:num2, third:num3, moneywin: userDBmoneyupdate }); // Send JSON response
+        });
+    });
+    
+
+   
+});
+
+
+app.post("/regist", (req, res) =>{
+    userrnick = "";
+    userrpass = "";
+
+    userrnick = req.body.usernick;
+    userrpass = req.body.userpass;
+
+
+    let sql = "SELECT * FROM utenti WHERE username = ?";
+    con.query(sql, [userrnick], function (err, result) {
+        if (err) {
+            console.error("Query error:", err);
+            return;
+        }
+    
+        if(result.length > 0){
+            res.json({respond:"ID:"+0+ ":User Already Used"})
+            return;
+
+        }
+
+
+        let updateSql = "INSERT utenti (username, userpassword, money) VALUES (?,?,?)";
+        con.query(updateSql, [userrnick, userrpass, 100], function (err, result) {
+            if (err) {
+                console.error("Query error:", err);
+                return;
+            }
+
+            let last_id = result.insertId;
+            
+            console.log("User added.");
+            res.json({respond:"ID:"+last_id});
+           
+        });
+    });
+
+
+});
+
+
+//login POST
+app.post("/login", (req, res) =>{
+    userrnick = "";
+    userrpass = "";
+
+    userrnick = req.body.usernick;
+    userrpass = req.body.userpass;
+
+
+    let sql = "SELECT * FROM utenti WHERE username = ? AND userpassword = ?";
+    con.query(sql, [userrnick,userrpass], function (err, result) {
+        if (err) {
+            console.error("Query error:", err);
+            return;
+        }
+    
+        if(result.length > 0){
+            console.log(result[0].ID)
+            res.json({respond:"ID:"+result[0].ID + ":money:"+result[0].money});
+            return;
+
+        }else{
+            res.json({respond: "ID:0"});
+            return;
+        }
+
+        
+        
+    });
+
+
+});
+
+
+
+
+
+//slot data POST
+app.post("/slotdata", (req, res) =>{
+
+
+    const userrid = req.body.userid;
+
+
+
+    let sql = "SELECT * FROM utenti WHERE ID = ?";
+    con.query(sql, [userrid], function (err, result) {
+        if (err) {
+            console.error("Query error:", err);
+            return;
+        }
+    
+        if(result.length > 0){
+            console.log(result[0].money)
+            res.json({usermoney:result[0].money});
+            return;
+
+        }else{
+            res.json({usermoney:"error"});
+            return;  
+        }
+
+        
+        
+    });
+
+
+});
+
+
+//slot redirect POST
+app.post("/slotredirect", (req, res) =>{
+    const page = req.body.pagesel;
+    res.json({ redirect: page });
+
+
+});
+
+app.listen(port, () => {
+    console.log(`Real: ${port}`);
+});
---
+++ slot1/js1/slotmachines.js
@@ -0,0 +1,303 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+var check = 0;
+async function meccanismo(){
+    
+
+
+    if(check == 0){
+
+        check = 1;
+
+        var useridjs = localStorage.getItem("userid");
+        betsizeselected = document.getElementById("betsizevalue").innerHTML = sizebet[sizebetposition];;
+        moneytot = document.getElementById("money").textContent.trim();
+        moneytot = Number(moneytot);
+        
+        if(moneytot < betsizeselected){
+
+            check = 0;
+            return;
+        }
+        else{
+            //var iduserjs = document.getElementById("iduserf").innerHTML;
+            //iduserjs = localStorage.getItem("userid");
+            $.ajax({
+                url: "http://192.168.1.1:3000/spin",
+                data: {id: useridjs, userbetsize: betsizeselected},
+                type: "POST",
+                success:async function(result){
+
+                    console.log(result);
+
+
+                    document.getElementById("money").innerHTML = moneytot-betsizeselected;
+
+                    document.getElementById("levabutton").disabled = true;
+                    document.getElementById("betsizeminus").disabled = true;
+                    document.getElementById("betsizeplus").disabled = true;
+            
+                    
+                    //console.log(moneytot);
+            
+                    rollosoundjs = document.getElementById("rollsound");
+                   
+                    jackpotsoundjs = document.getElementById("jackpotsound");
+            
+                    
+            
+                    document.getElementById("winspace").innerHTML = "<img id='winimage' src='img1/cyan.png' width='95%' height='80%' draggable='false'>";
+            
+                    rollosoundjs.currentTime = 0;
+                    rollosoundjs.play();
+                    
+            
+            
+            
+                    //Meccanismo
+                    conta = 0;
+            
+                
+            
+                    for(let conta = 0; conta < 33; conta++){
+                        if(conta < 25){
+                            num1 = Math.floor(Math.random() * 16) + 1;
+                            document.getElementById("space1").innerHTML = "<img src='img1/" + num1 + ".png'' width='100%' height='100%' draggable='false'>";
+                            //console.log("num1: "+num1);
+                            if(conta == 24){
+                                document.getElementById("space1").innerHTML = "<img src='img1/" + result.first + ".png'' width='100%' height='100%' draggable='false'>";
+                                
+                            }
+                        }
+                        
+
+                        if(conta < 28){
+                            num2 = Math.floor(Math.random() * 16) + 1;
+                            document.getElementById("space2").innerHTML = "<img src='img1/" + num2 + ".png' ' width='100%' height='100%' draggable='false'>";
+                        // console.log("num2: "+num2);
+                            if(conta == 27){
+                                document.getElementById("space2").innerHTML = "<img src='img1/" + result.second + ".png'' width='100%' height='100%' draggable='false'>";
+                            
+                            }
+                        }
+                       
+            
+                        num3 = Math.floor(Math.random() * 16) + 1;
+                        document.getElementById("space3").innerHTML = "<img src='img1/" + num3 + ".png' ' width='100%' height='100%' draggable='false'>";
+                        //console.log("num3= "+num3);
+                        if(conta == 32){
+                            document.getElementById("space3").innerHTML = "<img src='img1/" + result.third + ".png'' width='100%' height='100%' draggable='false'>";
+                            
+                        }
+                        await sleep(100);
+                    }   
+                    num1 = result.first;
+                    num2 = result.second;
+                    num3 = result.third;
+                    //
+                    
+                    //Win
+            
+                  
+            
+                    if(num1 == num2 && num2 == num3){
+                        
+                        jackpotsoundjs.play();
+            
+                        document.getElementById("winspace").innerHTML = "<img id='winimage' src='img1/jackpot.png'  width='95%' height='80%' draggable='false'>";
+                        console.log("win");
+            
+                        
+                        moneytot = result.moneywin;
+                    
+                        document.getElementById("money").innerHTML = Number(moneytot);
+                    
+                        
+             
+                    }
+                    else if(num1 == num3){
+            
+                        moneytot = result.moneywin;
+                    
+                        document.getElementById("money").innerHTML = Number(moneytot);
+                    
+                        
+       
+                    }
+                    else if(num1 == num2){
+                        
+                        moneytot = result.moneywin;
+                    
+                        document.getElementById("money").innerHTML = Number(moneytot);
+                    
+                        
+               
+            
+                    }
+                    else if(num2 == num3){
+                        moneytot = result.moneywin;
+                    
+                        document.getElementById("money").innerHTML = Number(moneytot);
+                    
+                        
+                
+            
+            
+                    }
+                    else{
+                        
+            
+                    }
+                    
+                    
+                    moneytot = result.moneywin;
+              
+                    moneytot = document.getElementById("money").innerHTML;
+                    moneytot = Number(moneytot);
+
+                    
+                    await sleep(400);
+                    
+                    document.getElementById("levabutton").disabled = false;
+                    document.getElementById("betsizeminus").disabled = false;
+                    document.getElementById("betsizeplus").disabled = false;
+                    check = 0;
+                    
+                },
+                error: function(xhr, status, error) {
+                    console.error("AJAX error:", status, error);
+                    check = 0; // Reset check on error
+                }
+                
+        
+        
+                
+            });
+        
+        }
+    
+    }   
+}
+
+window.onkeydown = function(event) {
+    if (event.key == 'Shift'){
+        autoroll();
+    }
+
+    if(check == 0){
+        if (event.key === 'Enter' ) {
+            meccanismo();
+        }
+        else if (event.key === 'ArrowRight' ) {
+             upbetsize();
+        }
+        else if (event.key === 'ArrowLeft' ) {
+            downbetsize();
+        }
+        else {
+
+        }
+            
+    
+    }
+};
+
+
+
+
+
+
+var checkauto = 0;
+async function autoroll(){
+
+    if(checkauto == 1){
+        checkauto = 0;
+        document.getElementById("autorollimg").src="img1/autorollbutton.png";
+    }
+    else if(checkauto == 0){
+        checkauto = 1;
+        document.getElementById("autorollimg").src="img1/autorollpressed.png";
+    }
+
+    while(checkauto == 1){
+        
+        await meccanismo();
+        await sleep(1000);
+
+        
+    }
+
+}
+
+
+var sizebetposition = 1;
+var sizebet = [0.5, 1, 5, 10, 25, 50, 100, 250, 1000,10000,100000];
+
+async function upbetsize(){
+    moneytot = document.getElementById("money").textContent.trim();
+    moneytot = Number(moneytot);
+
+    sizebetposition = sizebetposition + 1;
+    if(sizebetposition + 1 > sizebet.length){
+        sizebetposition = sizebetposition - 1
+    }
+        
+        
+    console.log(sizebetposition);
+    
+    document.getElementById("betsizevalue").innerHTML = sizebet[sizebetposition];
+}
+
+async function downbetsize(){
+
+    sizebetposition = sizebetposition - 1;
+    if(sizebetposition  < 0){
+        sizebetposition = sizebetposition + 1
+    }
+    console.log(sizebetposition);
+    
+    document.getElementById("betsizevalue").innerHTML = sizebet[sizebetposition];
+}
+
+
+
+
+
+
+
+//var usermoneyjs = localStorage.getItem("usermoney");
+
+async function startpageslot(){
+    
+    var useridjs = localStorage.getItem("userid");
+    
+    console.log(useridjs);
+    if(useridjs != 0){
+        $.ajax({
+            url: "http://192.168.1.1:3000/slotdata",
+            data: {userid: useridjs},
+            type: "POST",
+            success:function(result){
+                console.log(result.usermoney);
+
+                document.getElementById("money").innerText = result.usermoney;
+
+            }
+
+
+        });
+        //localStorage.setItem("userid", 0);
+    }
+    else{
+        window.location.href = "/index.html";
+       // localStorage.setItem("userid", 0);
+    }
+    document.getElementById("betsizevalue").innerHTML  = sizebet[sizebetposition];
+}
+
+async function returnhome() {
+    window.location.href = "/index.html";
+}
+
